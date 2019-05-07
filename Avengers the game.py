7d6940e84d8fc0f31a5a599d983df652eac66332
Pedro Celia <pedrodc1@al.insper.edu.br>
2019-05-07 11:50:19 -0300

    Inicio do jogo

--- Avengers the game.py
+++ Avengers the game.py
@@ -7,10 +7,171 @@ import random
 import time
 from os import path
 
+# Estabelece a pasta que contem as figuras e sons.
+img_dir = path.join(path.dirname(__file__), 'Avengers anime')
+fundo = path.join(path.dirname(__file__), 'Backgrounds')
+
+
+
+# Dados gerais do jogo.
+WIDTH = 1480 # Largura da tela
+HEIGHT = 700 # Altura da tela
+FPS = 60 # Frames por segundo
+
+# Define algumas variáveis com as cores básicas
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
+class Player(pygame.sprite.Sprite):
+    
+    # Construtor da classe.
+    def __init__(self, player_img):
+        
+        # Construtor da classe pai (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+        
+        # Carregando a imagem de fundo.
+        self.image = player_img
+        
+        # Diminuindo o tamanho da imagem.
+        self.image = pygame.transform.scale(player_img, (300, 250))
+        
+        # Deixando transparente.
+        self.image.set_colorkey(WHITE)
+        
+        # Detalhes sobre o posicionamento.
+        self.rect = self.image.get_rect()
+        
+        # Centraliza embaixo da tela.
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        
+        # Velocidade da nave
+        self.speedx = 0
+        
+        # Melhora a colisão estabelecendo um raio de um circulo
+        self.radius = 25
+        
+    def update(self):
+        self.rect.x += self.speedx
+        
+        # Mantem dentro da tela
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+            
+class Mob(pygame.sprite.Sprite):
+    
+    # Construtor da classe.
+    def __init__(self, mob_img):
+        
+        # Construtor da classe pai (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+        
+        # Diminuindo o tamanho da imagem.
+        self.image = pygame.transform.scale(mob_img, (50, 38))
+        
+        # Deixando transparente.
+        self.image.set_colorkey(BLACK)
+        
+        # Detalhes sobre o posicionamento.
+        self.rect = self.image.get_rect()
+        
+        # Sorteia um lugar inicial em x
+        self.rect.x = random.randrange(WIDTH - self.rect.width)
+        # Sorteia um lugar inicial em y
+        self.rect.y = random.randrange(-100, -40)
+        # Sorteia uma velocidade inicial
+        self.speedx = random.randrange(-3, 3)
+        self.speedy = random.randrange(2, 9)
+        
+        # Melhora a colisão estabelecendo um raio de um circulo
+        self.radius = int(self.rect.width * .85 / 2)
+        
+class Bullet(pygame.sprite.Sprite):
+    
+    # Construtor da classe.
+    def __init__(self, x, y, bullet_img):
+        
+        # Construtor da classe pai (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+        
+        # Carregando a imagem de fundo.
+        self.image = bullet_img
+        
+        # Deixando transparente.
+        self.image.set_colorkey(WHITE)
+        
+        # Detalhes sobre o posicionamento.
+        self.rect = self.image.get_rect()
+        
+        # Coloca no lugar inicial definido em x, y do constutor
+        self.rect.bottom = y
+        self.rect.centerx = x
+        self.speedy = -10
+
+    
+    def update(self):
+        self.rect.y += self.speedy
+        
+        # Se o tiro passar do inicio da tela, morre.
+        if self.rect.bottom < 0:
+            self.kill()
+
+def load_assets(img_dir):
+    Homem = path.join(img_dir, 'Iron Man')
+    Thanos = path.join(img_dir, 'Thanos')
+    fundo = path.join(path.dirname(__file__), 'Backgrounds')
+    assets = {}
+    assets["player_img"] = pygame.image.load(path.join(Homem, "Stance_Iron_Man.png")).convert()
+    assets["mob_img"] = pygame.image.load(path.join(Thanos, "Stance_Thanos.png")).convert()
+    assets["bullet_img"] = pygame.image.load(path.join(Homem, "Propulsor.png")).convert()
+    assets["background"] = pygame.image.load(path.join(fundo, 'City4.png')).convert()
+    return assets
+    
+pygame.init()
+pygame.mixer.init()
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+pygame.display.set_caption("Avengers the Game")
+
+assets = load_assets(img_dir)
+
+clock = pygame.time.Clock()
+
+background = assets["background"]
+background_rect = background.get_rect()
+
+player = Player(assets["player_img"])
+
+# Cria um grupo de todos os sprites e adiciona a nave.
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
+
+# Cria um grupo só dos meteoros
+mobs = pygame.sprite.Group()
+
+# Cria um grupo para tiros
+bullets = pygame.sprite.Group()
+
+# Cria 8 meteoros e adiciona no grupo meteoros
+for i in range(8):
+    m = Mob(assets["mob_img"])
+    all_sprites.add(m)
+    mobs.add(m)
+
+
 try:
     
     running = True
     while running:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -20,18 +181,51 @@ try:
                     player.speedx = -8
                 if event.key == pygame.K_RIGHT:
                     player.speedx = 8
-                if event.key == pygame.K_UP:
-                    player.speedx = 8
-                if event.key == pygame.K_DOWN:
-                    player.speedx = -8
-                    
-             if event.type == pygame.KEYUP:
-                if event.key == pygame.K_LEFT:
-                    player.speedx = 0
-                if event.key == pygame.K_RIGHT:
-                    player.speedx = 0
-                if event.key == pygame.K_UP:
-                    player.key = 0
-                if event.key == pygame.K_DOWN:
-                    player.key = 0
-            all_sprites.update()
+                if event.key == pygame.K_SPACE:
+                    bullet = Bullet(player.rect.centerx, player.rect.top, assets["bullet_img"])
+                    all_sprites.add(bullet)
+                    bullets.add(bullet)        
+#            if event.type == pygame.KEYUP:
+#                if event.key == pygame.K_LEFT:
+#                    player.speedx = 0
+#                if event.key == pygame.K_RIGHT:
+#                    player.speedx = 0
+#                if event.key == pygame.K_UP:
+#                    player.key = 0
+#                if event.key == pygame.K_DOWN:
+#                    player.key = 0
+           
+        all_sprites.update()
+            
+         # Verifica se houve colisão entre propulsor e Thanos
+        hits = pygame.sprite.groupcollide(mobs, bullets, True, True)
+        for hit in hits: # Pode haver mais de um
+            # O meteoro e destruido e precisa ser recriado
+            m = Mob(assets["mob_img"]) 
+            all_sprites.add(m)
+            mobs.add(m)
+        
+        # Verifica se houve colisão entre nave e meteoro
+        hits = pygame.sprite.spritecollide(player, mobs, False, pygame.sprite.collide_circle)
+        if hits:
+            # Toca o som da colisão
+            time.sleep(1) # Precisa esperar senão fecha
+            
+            running = False
+    
+        # A cada loop, redesenha o fundo e os sprites
+        screen.fill(BLACK)
+        screen.blit(background, background_rect)
+        all_sprites.draw(screen)
+        
+        # Depois de desenhar tudo, inverte o display.
+        pygame.display.flip()
+        
+finally:
+    
+    pygame.quit()
+    
+    
+    
+    
+    
