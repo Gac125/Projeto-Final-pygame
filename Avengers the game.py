d1fb0504d14cbc68813dc967b98d4727a94d37a6
Pedro Celia <pedrodc1@al.insper.edu.br>
2019-05-07 10:19:50 -0300

    inicie o looping principal

--- Avengers the game.py
+++ Avengers the game.py
@@ -2,4 +2,36 @@
 """
 Jogo Marvel - Giovanna Alves, Giulia Castro e Pedro Célia
 """
+import pygame
+import random
+import time
+from os import path
 
+try:
+    
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    player.speedx = -8
+                if event.key == pygame.K_RIGHT:
+                    player.speedx = 8
+                if event.key == pygame.K_UP:
+                    player.speedx = 8
+                if event.key == pygame.K_DOWN:
+                    player.speedx = -8
+                    
+             if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    player.speedx = 0
+                if event.key == pygame.K_RIGHT:
+                    player.speedx = 0
+                if event.key == pygame.K_UP:
+                    player.key = 0
+                if event.key == pygame.K_DOWN:
+                    player.key = 0
+            all_sprites.update()
