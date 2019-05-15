cdb128ef7ba4527efc8f7ce3bc0ccf26426dc194
giovannaapn <you@example.com>
2019-05-15 19:46:46 -0300

    Gravidade adicionada no arquivo da depre

--- arquivo_da_depressao.py
+++ arquivo_da_depressao.py
@@ -55,7 +55,7 @@ class Player(pygame.sprite.Sprite):
         # Melhora a colisão estabelecendo um raio de um circulo
         self.radius = 25
     def update(self):
-        self.acc = vec(0,0)
+        self.acc = vec(0,0.5)
         keys=pygame.key.get_pressed() 
         if keys[pygame.K_LEFT]:
             self.acc.x=-PLAYER_ACC
