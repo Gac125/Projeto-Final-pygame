f6acdd05a17342ffa78ba4897a264751f42ec4b5
Gac125 <giulia.castro12@gmail.com>
2019-05-31 11:35:21 -0300

    Tiros caindo de cima

--- arquivo_da_depressao.py
+++ arquivo_da_depressao.py
@@ -111,7 +111,39 @@ class Mob(pygame.sprite.Sprite):
 #        self.radius = int(self.rect.width * 1 / 2)    
     def update(self):
         self.rect.x += self.speedx
-        self.rect.y += self.speedy                   
+        self.rect.y += self.speedy    
+
+# Classe Mob que representa os meteoros
+class Mob2(pygame.sprite.Sprite):   
+    # Construtor da classe.
+    def __init__(self, mob2_img):        
+        # Construtor da classe pai (Sprite).
+        pygame.sprite.Sprite.__init__(self)        
+        # Diminuindo o tamanho da imagem.
+        self.image = pygame.transform.scale(mob2_img, (20, 38))        
+        # Deixando transparente.
+        self.image.set_colorkey(BLACK)     
+        # Detalhes sobre o posicionamento.
+        self.rect = self.image.get_rect()     
+        # Sorteia um lugar inicial em x
+        self.rect.x = random.randrange(WIDTH - self.rect.width)
+        # Sorteia um lugar inicial em y
+        self.rect.y = random.randrange(-100, -40)
+        # Sorteia uma velocidade inicial
+        self.speedx = random.randrange(-3, 3)
+        self.speedy = random.randrange(2, 9)       
+        # Melhora a colisão estabelecendo um raio de um circulo
+        self.radius = int(self.rect.width * .85 / 2)       
+    # Metodo que atualiza a posição do meteoro
+    def update(self):
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy        
+        # Se o meteoro passar do final da tela, volta para cima
+        if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or self.rect.right > WIDTH + 20:
+            self.rect.x = random.randrange(WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100, -40)
+            self.speedx = random.randrange(-3, 3)
+            self.speedy = random.randrange(2, 9)               
 
 class Bullet(pygame.sprite.Sprite):  
     # Construtor da classe.
@@ -154,6 +186,7 @@ def load_assets(img_dir):
     assets["background"] = pygame.image.load(path.join(fundo, 'houses31.png')).convert()
     assets["block_img"] = pygame.image.load(path.join(img_dir, 'Dano_Ultron.png')).convert()
     assets["buraco_img"] = pygame.image.load(path.join(img_dir, 'Buraco.png')).convert()
+    assets["tiro_img"] = pygame.image.load(path.join(img_dir, 'laserRed16.png')).convert()
     assets["score_font"] = pygame.font.Font(path.join(fnt_dir, "PressStart2P.ttf"), 28)
     return assets
 
@@ -190,7 +223,12 @@ def game_screen(screen):
         world_sprites.add(block)
         # Adiciona tambem no grupo de todos os sprites para serem atualizados e desenhados
         all_sprites.add(block)
-
+# Cria 8 meteoros e adiciona no grupo thanos
+    for a in range(5):
+        t = Mob2(assets["tiro_img"])
+        all_sprites.add(t)
+        mobs.add(t)
+        
     PLAYING = 0
     DONE = 1 
     score = 0
